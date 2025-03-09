ca171447bfce6defbbad78333a7b10d0d778a4e4
ricardond <rricardodias99@gmail.com>
2025-03-09 10:43:21 -0300

    Primeiro commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+export default function Navbar() {
+    return(
+        <header className='navbar text-amber-300 flex flex-col items-center justify-center'>
+            <h1 className="text-3xl font-bold">cardo.dev</h1>
+            <nav className="flex space-x-5 text-2xl items-center">
+                <Link className="hover:bg-amber-300 hover:text-gray-900 rounded-2xl p-2 transition-discrete duration-300" href='/'>Home</Link>
+                <Link className="hover:bg-amber-300 hover:text-gray-900 rounded-2xl p-2 transition-discrete duration-300" href='/habilidades'>Habilidades</Link>
+                <Link className="hover:bg-amber-300 hover:text-gray-900 rounded-2xl p-2 transition-discrete duration-300" href='/projetos'>Projetos</Link>
+            </nav>
+        </header>
+    );
+}
