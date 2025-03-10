7754f3476c0b930e09633dda63fd3753a4bbd87f
ricardond <rricardodias99@gmail.com>
2025-03-09 21:05:47 -0300

    Update index e card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
-import { Url } from "next/dist/shared/lib/router/router";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Cardprops{
     titulo: string;
@@ -10,7 +11,7 @@ interface Cardprops{
 export default function Card({titulo, desc, image, abrirProjeto}: Cardprops){
     return(
         <div className="flex flex-col w-90 h-90 max-w-90 min-w-50 rounded-lg p-3 cursor-pointer border border-gray-200 shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <img className="w-full h-50 rounded-2xl" src={image} alt="" />
+            <img className="w-full h-50 rounded-2xl" src={image} />
             <h2 className="text-amber-300 text-2xl">{titulo}</h2>
             <p className="text-white">{desc}</p>
             <a href={abrirProjeto} target="_blank" className="text-amber-500 rounded-2xl p-1 hover:bg-amber-500 hover:text-gray-800 border-1 border-amber-500 w-25">Ver projeto</a>
