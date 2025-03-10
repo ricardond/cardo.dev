5ee12c4439f5f93f210e893df15ef5ef685901c6
ricardond <rricardodias99@gmail.com>
2025-03-09 21:44:15 -0300

    update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 export default function Navbar() {
     return(
-        <header className='navbar text-amber-300 flex flex-col items-center justify-center'>
+        <header className='navbar text-amber-300 flex flex-col items-center justify-center fixed top-0 left-0 w-full bg-gray-900 z-50'>
             <h1 className="text-3xl font-bold">cardo.nd</h1>
             <nav className="flex space-x-5 text-2xl items-center">
-                <Link className="hover:bg-amber-300 hover:text-gray-900 rounded-2xl p-2 transition-discrete duration-300" href='/'>Home</Link>
-                <Link className="hover:bg-amber-300 hover:text-gray-900 rounded-2xl p-2 transition-discrete duration-300" href='/habilidades'>Habilidades</Link>
-                <Link className="hover:bg-amber-300 hover:text-gray-900 rounded-2xl p-2 transition-discrete duration-300" href='/portfolio'>Portfólio</Link>
+            <Link className="hover:bg-amber-300 hover:text-gray-900 rounded-2xl p-2 transition-discrete duration-300" href='/'>Home</Link>
+            <Link className="hover:bg-amber-300 hover:text-gray-900 rounded-2xl p-2 transition-discrete duration-300" href='/habilidades'>Habilidades</Link>
+            <Link className="hover:bg-amber-300 hover:text-gray-900 rounded-2xl p-2 transition-discrete duration-300" href='/portfolio'>Portfólio</Link>
             </nav>
         </header>
     );
