1d6e149753fccf3c09f1ddeb058d218378e5042f
ricardond <rricardodias99@gmail.com>
2025-03-09 22:34:17 -0300

    atialização index novamente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <section className="flex flex-wrap-reverse justify-center items-center flex-wrap gap-5 m-40"> 
+      <section className="flex flex-col-reverse justify-center items-center flex-wrap gap-5 mt-25"> 
         <div className="flex flex-col flex-wrap">
           <h1 className="text-amber-300 text-3xl font-bold">Olá! Me chamo Ricardo Dias, eu desenvolvo coisas!</h1>
           <p className="text-amber-50 text-2xl">Sou graduado em Computação, sou Desenvolvedor Web Full-Stack<br/> e nas horas vagas desenvolvo alguns games</p>
