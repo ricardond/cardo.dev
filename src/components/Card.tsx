af172bd516f3e4539382c583f05a84753f79a46d
ricardond <rricardodias99@gmail.com>
2025-03-09 21:14:39 -0300

    Corrção no footer

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 
 interface Cardprops{
     titulo: string;
@@ -11,7 +10,7 @@ interface Cardprops{
 export default function Card({titulo, desc, image, abrirProjeto}: Cardprops){
     return(
         <div className="flex flex-col w-90 h-90 max-w-90 min-w-50 rounded-lg p-3 cursor-pointer border border-gray-200 shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <img className="w-full h-50 rounded-2xl" src={image} />
+            <Image className="w-full h-50 rounded-2xl" src={image} alt={titulo} width={500} height={300} />
             <h2 className="text-amber-300 text-2xl">{titulo}</h2>
             <p className="text-white">{desc}</p>
             <a href={abrirProjeto} target="_blank" className="text-amber-500 rounded-2xl p-1 hover:bg-amber-500 hover:text-gray-800 border-1 border-amber-500 w-25">Ver projeto</a>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
+
 export default function Footer() {
     return(
         <footer className="bg-white rounded-lg shadow-sm dark:bg-gray-700 mt-5">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <Link href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ricardo Dias</span>
-            </a>
+            </Link>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
                     <a href="#" className="hover:underline me-4 md:me-6">Sobre mim</a>
