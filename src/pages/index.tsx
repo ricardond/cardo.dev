3625e8e8a5a761a67c3c4e3ad3a9a1e244cbbe41
ricardond <rricardodias99@gmail.com>
2025-03-09 22:05:04 -0300

    atualização da margin-top

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <section className="flex flex-wrap-reverse items-center justify-around m-20"> 
+      <section className="flex flex-wrap-reverse items-center justify-around mt-20"> 
         <div className="flex flex-col flex-wrap">
           <h1 className="text-amber-300 text-3xl font-bold">Olá! Me chamo Ricardo Dias, eu desenvolvo coisas!</h1>
           <p className="text-amber-50 text-2xl">Sou graduado em Computação, sou Desenvolvedor Web Full-Stack<br/> e nas horas vagas desenvolvo alguns games</p>
