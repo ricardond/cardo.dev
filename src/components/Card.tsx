fa5e8e37cc6b42cccbdeac075c9084846c0b54b7
ricardond <rricardodias99@gmail.com>
2025-03-14 17:39:43 -0300

    Update css

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ interface Cardprops{
 
 export default function Card({titulo, desc, image, abrirProjeto}: Cardprops){
     return(
-        <div className="flex flex-col w-90 h-90 max-w-90 min-w-50 rounded-lg p-3 cursor-pointer border border-gray-200 shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div className="flex flex-col w-90 h-90 max-w-90 min-w-50 rounded-lg p-3 cursor-pointer border border-gray-200 shadow-sm hover:bg-gray-800 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <Image className="w-full h-50 rounded-2xl" src={image} alt={titulo} width={500} height={300} />
             <h2 className="text-amber-300 text-2xl">{titulo}</h2>
             <p className="text-white">{desc}</p>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
     return(
         <footer className="bg-gray-800 text-white text-center py-4 fixed bottom-0 w-full">
-            <p>Ricardo Dias. All rights reserved.</p>
+            <p>Ricardo Dias. Todos os direitos reservados.</p>
             <a href="https://github.com/ricardond" className="text-blue-500 hover:text-blue-700">GitHub</a>
         </footer>   
     )
